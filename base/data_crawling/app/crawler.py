7e31b24812dac088370e050769c0f1ec79b9dae3
vinhthai2905 <vinhthai2905@aol.com>
2025-05-15 17:35:24 +0700

    50%

--- base/data_crawling/app/crawler.py
+++ base/data_crawling/app/crawler.py
@@ -19,15 +19,60 @@ else:
     try:
         for i, product in enumerate(phoneInfos.find_all('li', class_='item ajaxed __cate_42')):
             productHref = product.find('a')
-            productImage = product.find
+            productBanners = product.find('div', class_='item-img item-img_42')
             productTags = productHref.find('div', class_='item-label')
             productNew = productTags.find('span', class_='ln-new')
-            productInstallment = productTags.find('span', class_='lb-tragop')
+            productInstallment = productTags.find('img', class_='lb-tragop')
             
+            productName = product.find('h3').text
+            productTech = product.find('div', class_='item-compare gray-bg').text
+            
+            print(productTech)
+            # productDeals = product.find('p', class_='result-label temp1')
+            # if productDeals:
+            #     dealIcon = productDeals.find('img').get('data-src')
+            #     if dealIcon == None:
+            #         dealIcon = productDeals.find('img').get('src')
+                    
+            #     dealText = productDeals.find('span')
+                
+            # elif productDeals.find('result-label temp5') == None:            
+            #     dealText = 
+            # elif productDeals.find('result-label temp6') == None:
+            #     pass
+            # elif productDeals.find('p', class_='preoder') == None:
+            #     pass
+            
+            # dealIcon = productDeals.find('img').get('data-src')
+            # if dealIcon == None:
+            #     dealIcon = productDeals.find('img').get('src')
+                
+            
+                
+                
+            imageTags = productBanners.find_all('img')
+            
+            if len(imageTags) == 2:
+                productImageURL = imageTags[0].get('data-src')
+                if productImageURL == None:
+                    productImageURL = imageTags[0].get('src')
+                    
+                exclusiveTag = imageTags[1].get('data-src')
+                if exclusiveTag == None:
+                    exclusiveTag = imageTags[1].get('src')
+            else:
+                exclusiveTag = None
+                productImageURL = imageTags[0].get('data-src')
+                if productImageURL == None:
+                    productImageURL = imageTags[0].get('src')
+                
             if productNew:
                 productNew = productNew.text
+                
             if productInstallment:
                 productInstallment = productInstallment.text
+                
+                
             
     except Exception as e:
         print(traceback.print_exception(type(e), e, e.__traceback__))
