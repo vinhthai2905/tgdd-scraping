0c52ae7e7c8be3fe201e07858244facf5b11965d
vinhthai2905 <lethihtrang998877@gmail.com>
2025-05-17 11:29:44 +0700

    feat: improved handling errors while crawling

--- base/data_crawling/main.py
+++ base/data_crawling/main.py
@@ -6,14 +6,13 @@ from app.scheduler import schedule_phone_crawling
 app = FastAPI()
 
 
-@app.on_event("startup")
-def start_scheduler():
-    thread = Thread(target=schedule_phone_crawling)
-    thread.daemon = True
-    thread.start()
+# @app.on_event("startup")
+# def start_scheduler():
+#     thread = Thread(target=schedule_phone_crawling)
+#     thread.daemon = True
+#     thread.start()
 
 
-@app.get('/dtdd')
+@app.get('/crawling/dtdd/')
 def datas_crawling():
-    phone_crawling()
-    return {'message': 'Crawling started. Check logs for more information'}
+    return {'Message': phone_crawling()}
